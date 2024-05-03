3b9d38b063f4e3ac5b4f4bedc6b5aced19218d1d
valentinotuksa <valentinotuksa@protonmail.com>
2024-05-03 22:41:08 +0200

    Add test steps with descriptions

--- tests/sign-up-page.spec.ts
+++ tests/sign-up-page.spec.ts
@@ -7,18 +7,34 @@ test.describe.parallel("Validate that the sign up page is working", () => {
   let signUpPage: SignUpPage;
 
   test.beforeEach(async ({ page }) => {
-    signUpPage = new SignUpPage(page);
-    await signUpPage.goto();
+    await test.step("Go to the sign up page", async () => {
+      signUpPage = new SignUpPage(page);
+      await signUpPage.goto();
+    });
   });
 
   test("Check if user can sign up with valid credentials", async ({ page }) => {
-    await signUpPage.usernameInput.fill(GenerateUser.username);
-    await signUpPage.emailInput.fill(GenerateUser.email);
-    await signUpPage.passwordInput.fill(GenerateUser.password);
-    await signUpPage.submitButton.click();
-    const signedInHomePage = new SignedInHomePage(page);
-    await expect(
-      signedInHomePage.signedInHeader.newArticleNavButton,
-    ).toBeVisible();
+    await test.step("Enter the username", async () => {
+      await signUpPage.usernameInput.fill(GenerateUser.username);
+    });
+
+    await test.step("Enter the email", async () => {
+      await signUpPage.emailInput.fill(GenerateUser.email);
+    });
+
+    await test.step("Enter the password", async () => {
+      await signUpPage.passwordInput.fill(GenerateUser.password);
+    });
+
+    await test.step("Submit the form", async () => {
+      await signUpPage.submitButton.click();
+    });
+
+    await test.step("Check if the user is signed in", async () => {
+      const signedInHomePage = new SignedInHomePage(page);
+      await expect(
+        signedInHomePage.signedInHeader.newArticleNavButton,
+      ).toBeVisible();
+    });
   });
 });

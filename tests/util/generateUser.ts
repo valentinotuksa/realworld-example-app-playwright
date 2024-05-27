6f3038e5e686a8a085c6cb9e7cd77da5c4774545
valentinotuksa <valentinotuksa@protonmail.com>
2024-05-28 01:19:56 +0200

    Removing experimenting code

--- tests/util/generateUser.ts
+++
@@ -1,6 +0,0 @@
-//@TODO: find a better way? maybe a faker library?
-export class GenerateUser {
-  static email: string = `test${Date.now().toString()}@fake.com`;
-  static username: string = `Test${Date.now().toString()}`;
-  static password: string = "1234";
-}
